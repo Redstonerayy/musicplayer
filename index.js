fdc34fde59345fc62ec99819d87eed36bc91dbca
Redstonerayy <U25a5DM501B9SH5r0@protonmail.com>
2020-12-28 23:05:31 +0100

    Added howler.js and a song class

--- index.js
+++ index.js
@@ -12,7 +12,71 @@ exportpath = "";
 ============================================================== */
 
 
-button = document.getElementsByTagName('button')[0];
+// TODO: play, pause, resume, volume, loop, random, timestamp, songtext
+
+
+
+/* ==============================================================
+                      CLASSES
+============================================================== */
+
+
+// plays the current song and makes pause usw. possible
+// there will only one song, the current song that is played
+// wrapped Howl methods in my own class
+function song(filename){
+  this.song = new Howl({
+    src: [filename]
+  });
+  this.duration = this.song.duration();
+
+  //methods
+  this.playorresume = () => {
+    this.song.once('load', () => {
+      this.song.play();
+    });
+  }
+
+  this.pause = (reset) => {
+    if(reset){
+      this.song.stop();
+    } else {
+      this.song.pause();
+    }
+  }
+
+  this.mute = (mute) => { // true -> mute; false -> unmute
+    this.song.mute(mute);
+  }
+
+  this.loop = (loop) => { // true -> loop; false -> unloop
+    this.song.loop(loop);
+  }
+
+  this.setvolume = (volume) => { //0.0 - 1.0
+    this.song.volume(volume);
+  }
+
+  this.setrate = (rate) => { //0.5 - 4.0
+    this.song.rate(rate);
+  }
+
+  this.settime = (seconds) => {
+    this.song.seek(seconds);
+  }
+}
+
+
+/* ==============================================================
+                      APP
+============================================================== */
+
+
+/* dump  */
+button = document.getElementById('export');
+
+
+
 
 button.addEventListener('click', () => {
   var sound = new Howl({
